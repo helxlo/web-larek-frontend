24f556568f99e8a82a8048221003db253c2669e2
helxlo <tommo13@ya.ru>
2024-03-13 08:30:56 +0300

    13/03

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,29 @@
-interface CardDara {
+export interface CardData {
     id: string;
-    description: string;
+    description?: string;
     image: string;
     title: string;
     category: string;
     price: number | null; 
 }
 
-interface UserData {
+export interface UserData {
     payment: boolean;
     email: string;
     phone: string;
     address: string;
     total: number;
-    items: string[];
 }
+
+export interface IAppState {
+    catalog: CardData[];
+    basket: string[];
+    preview: string | null;
+    order: IOrder | null;
+}
+
+export interface IOrder extends UserData {
+    items: string[]
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
